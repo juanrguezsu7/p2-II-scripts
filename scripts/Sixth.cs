d3d3e78a4f7c52007f7d258be6a9cd3fa693e1ac
Juan Rodríguez Suárez <alu0101477596@ull.edu.es>
2024-10-07 17:11:59 +0100

    Updated

--- scripts/Sixth.cs
+++ scripts/Sixth.cs
@@ -4,32 +4,18 @@ using UnityEngine;
 
 public class Sixth : MonoBehaviour
 {
-    public GameObject[] firstObjectDestiny = new GameObject[2];
-    public GameObject[] secondObjectDestiny = new GameObject[2];
-    public GameObject[] thirdObjectDestiny = new GameObject[2];
-    Transform[] firstTransform = new Transform[2];
-    Transform[] secondTransform = new Transform[2];
-    Transform[] thirdTransform = new Transform[2];
+    public Vector3 objective;
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 2; i++)
-        {
-            firstTransform[i] = firstObjectDestiny[i].transform;
-            secondTransform[i] = secondObjectDestiny[i].transform;
-            thirdTransform[i] = thirdObjectDestiny[i].transform;
-        }
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            firstTransform[0].Translate(firstTransform[1].position);
-            secondTransform[0].Translate(secondTransform[1].position);
-            thirdTransform[0].Translate(thirdTransform[1].position);
-        }
+      if (Input.GetAxis("Jump") > 0) {
+        transform.Translate(objective * Time.deltaTime, Space.Self);
+      }
     }
 }
--- scripts/Third.cs
+++ scripts/Third.cs
@@ -6,25 +6,37 @@ public class Third : MonoBehaviour
 {
     public Vector3 firstVector;
     public Vector3 secondVector;
+    public double magnitudeFirstVector;
+    public double magnitudeSecondVector;
+    public double angleBetweenVectors;
+    public double distanceBetweenVectors;
+    public string higherVector;
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Magnitude of firstVector: " + firstVector.magnitude);
+        magnitudeFirstVector = firstVector.magnitude;
         Debug.Log("Magnitude of secondVector: " + secondVector.magnitude);
+        magnitudeSecondVector = secondVector.magnitude;
         Debug.Log("Angle between firstVector and secondVector: " + Vector3.Angle(firstVector, secondVector));
+        angleBetweenVectors = Vector3.Angle(firstVector, secondVector);
         Debug.Log("Distance between firstVector and secondVector: " + Vector3.Distance(firstVector, secondVector));
+        distanceBetweenVectors = Vector3.Distance(firstVector, secondVector);
         if (firstVector.y > secondVector.y)
         {
             Debug.Log("firstVector is higher than secondVector");
+            higherVector = "First Vector";
         }
         else if (firstVector.y < secondVector.y)
         {
             Debug.Log("secondVector is higher than firstVector");
+            higherVector = "Second Vector";
         }
         else
         {
             Debug.Log("firstVector and secondVector are at the same height");
+            higherVector = "Same Height";
         }
     }
 }
